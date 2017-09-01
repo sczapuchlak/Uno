5e27d1bc4ccdf1923ed51a687c86f9ff66cf5de1
SH <email@email.com>
2017-08-31 20:43:31 -0500

    began Player class, tested and not working yet. SH

--- Uno_objects.py
+++ Uno_objects.py
@@ -1,3 +1,27 @@
 class Player:
+    #write the class definition, #__ = private.
+    def __init__(self, name): #this method initializes the attributes
+        self.__name = name
 
-    def __init__(self):
+    def set_name(self, name):#method accepts argument for player's name
+        self.__name = name
+
+    # def set_hand(self, hand):# is this a thing? hand will need to be a dictionary
+    #     self.__hand = hand{:} #maybe make this a dictionary?
+
+    def get_name(self): #method RETURNS the player's name
+        return
+
+    # def get_hand(self): #method needed to return hand dictionary, NEEDS WORK>
+
+#to do/HAND? - add dealt hand, play a card, add the discarded cards
+#clear() will clear the dictionary
+#pop-specific value returned/ popitem- random value returned/ items shows all keys/values
+
+#for testing...not working
+def main():
+    my_name = Player("Sonja")
+    print(my_name)
+
+if __name__ == '__main__':
+    main

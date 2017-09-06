9d9bc60f63eb142e5a28473eeb86d3b27fef77f1
SH <email@email.com>
2017-09-06 16:04:45 -0500

    One player works, can print deck and player hand, -sh

--- Uno_objects.py
+++ Uno_objects.py
@@ -1,7 +1,10 @@
+from UnoCardClass import Deck, Hand, Card
+
 class Player(object):
     #write the class definition, #__ = private.
-    def __init__(self, name): #this method initializes the attributes
+    def __init__(self, name, hand): #this method initializes the attributes
         self._name = name
+        self._hand = hand
 
     # def set_name(self, name):#method accepts argument for player's name
     #     self.__name = name NOT NEEDED.
@@ -11,6 +14,11 @@ class Player(object):
     @property
     def name(self): #method RETURNS the player's name
         return self._name
+
+    # def draw_card(self, deck):
+
+
+    # def play_card(self):
 
     # def get_hand(self): #method needed to return hand dictionary, NEEDS WORK>
 
@@ -25,13 +33,14 @@ class Player(object):
 def main():
     #create the deck from the Deck class
     uno_deck = Deck()
-
+    print(uno_deck)#testing.  it works.
     player_name = input("Ready to play! What's yer name? ")
     #validation, check for empty string
-    current_player = Player(player_name)#create an object from the Player class
+    current_player = Player(player_name, Hand())#create an object from the Player class
     print("Welcome", current_player.name, "Let's Play Uno...")
-    #create an UNO deck object from the Deck class
-    # print(my_name)
+    #create a hand for the current player
+    uno_deck.drawCards(current_player._hand, 5)
+    print(current_player._hand)
     # print("Sonja")
     # print(my_name)
 
